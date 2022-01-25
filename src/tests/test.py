6d477efcb15e2accaf57b365f28f425260052747
xiaoqixian <2100125584@qq.com>
2022-01-26 00:56:50 +0800

    trying to reconstruct the whole project

--- src/tests/test.py
+++ src/tests/test.py
@@ -6,6 +6,23 @@
 # > Location        : Shanghai
 # > Copyright@ https://github.com/xiaoqixian
 
-a = "hello"
+# a = [1,2,3,4,5,7,8,9]
+# b = [1,3,5]
+# print a[:]
+# print a[1:]
+# print a[:2]
+# print a[1:4]
+# print a[b]
 
-print a.upper()
+# """
+# Slice0: 30
+# Slice1: 31
+# Slice2: 32
+# Slice3: 33
+# """
+
+def func(a, b = 3):
+    print a + b
+
+func(1, 2)
+func(1)
